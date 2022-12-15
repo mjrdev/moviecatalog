17eb3e3a0c71fc0793b26762ad032161a939e913
mjrdev <jrgomesdev@gmail.com>
2022-12-14 22:31:54 -0300

    fix: conflit

--- src/http/models/MovieModel.php
+++ src/http/models/MovieModel.php
@@ -2,23 +2,6 @@
 namespace MovieCatalog\Http\Models;
 
 class MovieModel {
-<<<<<<< HEAD
-  private static function init() {
-    CONN->query('CREATE TABLE IF NOT EXISTS movies (
-      id bigint AUTO_INCREMENT PRIMARY KEY,
-      title varchar(1000) NOT NULL,
-      description text NOT NULL,
-      url_image text NOT NULL
-    );');
-
-    $verifyIfExistsInitalValues = CONN->query('SELECT * FROM movies;');
-    if($verifyIfExistsInitalValues->num_rows == 0) {
-      require('initialValues.php');
-      CONN->query($queryInsertValues);
-    }
-  }
-=======
->>>>>>> docker
 
   public static function create(): mixed {
     $title = $_POST['title']; $description = $_POST['description']; $url = $_POST['url_image'];
